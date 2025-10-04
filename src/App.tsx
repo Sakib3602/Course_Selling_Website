b32f112f2739cce9867d74dc005429f4d5ec9092
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-04 11:18:30 +0600

    basic setup done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+function App() {
+  
+
+  return (
+    <>
+    <p className='text-9xl '>Hello</p>
+      <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
